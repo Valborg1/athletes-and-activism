db242bca0a8efa658c9a7e359e18f5d4951f0fbe
Sal Gamez <gamezsalvador@gmail.com>
2021-02-24 20:36:54 -0700

    fixed error in charity page

--- client/src/pages/Charities.js
+++ client/src/pages/Charities.js
@@ -1,30 +1,16 @@
-
 import React from "react";
 import { Row, Col, Container } from "../components/Grid";
 import { Form, Button } from "react-bootstrap";
 import "./style.css";
 
-import React from 'react';
-// import { } from 'react-bootstrap/Image'
-import { Row, Col, Container } from '../components/Grid';
-import CausesList from '../components/CausesList';
-import CausesDesc from '../components/CausesDesc';
-import AthleteCharities from '../components/AthleteCharities'
-import AthleteCauses from '../components/AthleteCauses';
-import AthleteList from '../components/AthleteList'
-import CausesURL from '../components/CausesURL';
-import "./style.css"
-
-
 export default function Causes() {
   return (
     <>
-
       <Container title="title">
         <Row>
           <Col size="md-2" />
           <Col size="md-8">
-            <h1 className="text-center">CHARITIES</h1>
+            <h1 className="text-center">CHARITY</h1>
           </Col>
           <Col size="md-2">
             <button className="like btn" type="button">
@@ -74,51 +60,3 @@ export default function Causes() {
     </>
   );
 }
-
-    <Container title="title">
-        <Row>
-        <div class="form-outline">
-        <input type="search" id="form1" class="form-control" placeholder="Type query"
-        aria-label="Search" />
-        </div>
-    </Row>
-    <Row>    
-        <Col size="md-2"/>
-        <Col size="md-8">
-            <h1 className="text-center">CHARITIES</h1>
-        </Col>
-        <Col size="md-2">
-            <button className="like btn" type="button"><i className="fa fa-heart"></i></button>
-            <button className="update btn" type="button"><i className="fa fa-plus"></i></button>
-        </Col>
-    </Row>
-</Container>
-
-<Container>
-<div className="row g-0">
-  <div className="col-sm-6 col-md-8">CHARITY</div>
-
-  <div className="col-6 col-md-4">POPULAR</div>
-</div>
-<div className="container px-4">
-  <div className="row gx-5">
-    <div className="col">
-     <div className="p-3 border bg-light">{ CausesList }</div>
-     
-    </div>
-    <div className="col">
-      <div className="p-3 border bg-light">{}</div>
-    </div>
-  </div>
-</div>
-</Container>
-
-</>
-
-
-    
-
-    );
-}
-
-
