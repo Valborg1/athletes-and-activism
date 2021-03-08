16c3a18374de85f957988caebe1f987f4688f83e
Schneider Miner <schneiderminer@Schneiders-Air.hsd1.ut.comcast.net>
2021-03-08 12:56:02 -0700

    trying to add the functionality of querying our db first before we query the third party so that we do not have multiple athletes in db.

--- client/src/pages/AthleteSearch.js
+++ client/src/pages/AthleteSearch.js
@@ -2,17 +2,24 @@ import React, { useState, useEffect, InputGroup, FormControl } from 'react'
 import { Row, Col, Container } from "../components/Grid"
 import AthleteBio from "../components/AthleteBio"
 import Image from 'react-bootstrap/Image'
-import { Link, useParams } from "react-router-dom";
-import AthleteCharities from '../components/AthleteCharities'
-import AthleteCauses from '../components/AthleteCauses'
 import "./style.css"
-import AthleteBars from '../components/AthleteBars'
 import API from "../utils/API"
-import CardBtn from "../components/CardBtn"
 
 export default function Athletes(props) {
 
     const [athlete, setAthlete] = useState({
+        idPlayer: "",
+        dateBorn: "",
+        strPlayer: "",
+        strSport: "",
+        strTeam: "",
+        strPosition: "",
+        strCutout: "",
+        strThumb: "",
+        strDescriptionEN: "",
+    })
+
+    const [athleteDB, setAthleteDB] = useState({
         idPlayer: "",
         dateBorn: "",
         strPlayer: "",
@@ -40,7 +47,7 @@ export default function Athletes(props) {
         setSearch({ [name]: value });
     }
 
-   
+
     function _createAthleteInDB() {
         const data = {
             playerid: athlete.idPlayer,
@@ -51,7 +58,7 @@ export default function Athletes(props) {
             dob: athlete.dob,
             bio: athlete.bio,
         }
-            
+
         API.createAthlete(data)
             .then(res => {
                 console.log("create Athlete Response", res);
@@ -59,40 +66,37 @@ export default function Athletes(props) {
             })
 
     }
-    
+
 
     function _handleSearch(event) {
         event.preventDefault()
 
-        // console.log("fullName", fullName)
-        // console.log("firstName", search.firstName)
-        // console.log("lastName", search.lastName)
-        // API.getAthlete()
-        // .then(res => {
-        //     console.log("single athlete res", res.data)
-        //     // if (res.data === search.name){
-        //     //     window.location = `/add-athlete/${res.data.playerid}`
-        //     // }
-        //      return res.data
-        // })
+        
+        API.getAthleteDB(search)
+            .then(res => { 
+                setAthlete(res.data);
+                console.log("search", search); 
+                console.log("Database results", res);
+                if (res.status === 200) {window.location = `/add-athlete/${res.data[0].playerid}`}
+        })
 
-        API.searchAthletes(search)
-            .then(res => {
-                var description = res.data.player[0].strDescriptionEN.split(" ").splice(0, 50).join(" ") + "...";
-                setAthlete({
-                    idPlayer: res.data.player[0].idPlayer,
-                    dob: res.data.player[0].dateBorn,
-                    strPlayer: res.data.player[0].strPlayer,
-                    sport: res.data.player[0].strSport,
-                    team: res.data.player[0].strTeam,
-                    strCutout: res.data.player[0].strCutout,
-                    strThumb: res.data.player[0].strThumb,
-                    bio: description,
-                })
-                setShow({
-                    isActive: true
-                })
-            })
+        // API.searchAthletes(search)
+        //     .then(res => {
+        //         var description = res.data.player[0].strDescriptionEN.split(" ").splice(0, 50).join(" ") + "...";
+        //         setAthlete({
+        //             idPlayer: res.data.player[0].idPlayer,
+        //             dob: res.data.player[0].dateBorn,
+        //             strPlayer: res.data.player[0].strPlayer,
+        //             sport: res.data.player[0].strSport,
+        //             team: res.data.player[0].strTeam,
+        //             strCutout: res.data.player[0].strCutout,
+        //             strThumb: res.data.player[0].strThumb,
+        //             bio: description,
+        //         })
+        //         setShow({
+        //             isActive: true
+        //         })
+        //     })
     }
 
     return (
@@ -113,59 +117,51 @@ export default function Athletes(props) {
                                 onChange={_handleNameChange} />
                             <button className="btn blue">Search</button>
                         </form>
-                        {/* <InputGroup className="athlete-search">
-                            <InputGroup.Prepend>
-                                <InputGroup.Text>First and last name</InputGroup.Text>
-                            </InputGroup.Prepend>
-                            <FormControl 
-                                placeholder="Enter first name"
-                                type="text"
-                                name="search"
-                                value={search.name}
-                                onChange={_handleChange}/>
-                            <FormControl />
-                        </InputGroup> */}
                     </Col>
                 </Row>
             </Container>
+{/* Conditional render to populate the player's name */}
             {  show.isActive &&
-            <Container title="title">
-                <Row>
-                    <Col size="md-2"/>
-                    <Col size="md-8">
-                        <h1 className="text-center">{athlete.strPlayer}</h1>
-                    </Col>
-                </Row>
-            </Container>}
-            {  show.isActive && <Container 
-                title="stats">   
-                <Row>
-                    <Col size="md-4">
-                        <Image className="adjust-picture img-fluid" 
-                        placeholder="https://via.placeholder.com/25" 
-                        src={athlete.strCutout} roundedCircle />
-                    </Col>
-                    <Col size="md-8">
-                        <AthleteBio
-                            data={athlete}
-                        />
-                    </Col>
-                </Row>
-            </Container> }
+                <Container title="title">
+                    <Row>
+                        <Col size="md-2" />
+                        <Col size="md-8">
+                            <h1 className="text-center">{athlete.strPlayer}</h1>
+                        </Col>
+                    </Row>
+                </Container>}
+{/* Conditional render to populate the player's picture and stats */}
             {  show.isActive &&
-            <Container title="no-background">
-                <Row>
-                    <Col size="md-12">
-                        <button
-                            className="btn blue"
-                            id="add-athlete-btn"
-                            onClick={_createAthleteInDB}
+                <Container title="stats">
+                    <Row>
+                        <Col size="md-4">
+                            <Image className="adjust-picture img-fluid"
+                                alt="Athlete's picture"
+                                src={athlete.strCutout} roundedCircle />
+                        </Col>
+                        <Col size="md-8">
+                            <AthleteBio
+                                data={athlete}
+                            />
+                        </Col>
+                    </Row>
+                </Container>
+            }
+{/* Conditional render to populate the button to add player to the database. */}
+            {  show.isActive &&
+                <Container title="no-background">
+                    <Row>
+                        <Col size="md-12">
+                            <button
+                                className="btn blue"
+                                id="add-athlete-btn"
+                                onClick={_createAthleteInDB}
 
-                        >Add athlete to the database</button>
-                    </Col>
-                </Row>
-            </Container> }
-
+                            >Add athlete to the database</button>
+                        </Col>
+                    </Row>
+                </Container>
+            }
         </>
     )
 };
